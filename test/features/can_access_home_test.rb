18626ab837f87da6f0d7de77dd7d62c2ed319e41
SupahNickie <nicholascase@live.com>
2013-10-09 10:31:26 -0700

    Fixing screenshot screwup from before

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 feature "CanAccessHome" do
-  scenario "visiting the home page" do
+  scenario "visiting the home page to make sure it works" do
     visit root_path
     page.must_have_content "Hello World"
     page.wont_have_content "Goodbye All!"
