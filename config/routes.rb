0a869cd4cf60c1f62f4abb591fc7a92afd5d7066
SupahNickie <nicholascase@live.com>
2013-10-09 13:00:10 -0700

    CRUD scaffold created, tests all passing and refactored with yaml, fixes #11, #12, #13, #14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Portfolio::Application.routes.draw do
 
+  resources :posts
+
+
   root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
