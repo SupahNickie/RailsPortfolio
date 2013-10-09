850f117883d60147edcfd3f6d121fa373619a06f
SupahNickie <nicholascase@live.com>
2013-10-09 13:10:06 -0700

    CRUD assignment, Oct 9, Fixes #11, #12, #13, #14

--- test/features/post_create_test.rb
+++ test/features/post_create_test.rb
@@ -5,14 +5,14 @@ feature "creating a post" do
     # Given that a completed post form
     visit posts_path
     click_on 'New Post'
-    fill_in 'Title', with: 'a new hope'
+    fill_in 'Title', with: 'a new hope yay'
     fill_in 'Content', with: 'a long time ago in a galaxy far far away'
 
     # When the user submits the form
     click_on 'Create Post'
 
     # Then the user should see the new post and a success message
-    page.text.must_include 'a new hope'
+    page.text.must_include 'a new hope yay'
     page.text.must_include 'a long time ago in a galaxy far far away'
     page.text.must_include 'Post was successfully created'
 
