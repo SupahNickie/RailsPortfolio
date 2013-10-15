869c3a05928e483cc91b4a0e437a51adf04ede3c
SupahNickie <nicholascase@live.com>
2013-10-14 19:16:42 -0700

    Heroku fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131009191731) do
+ActiveRecord::Schema.define(:version => 20131015021121) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
