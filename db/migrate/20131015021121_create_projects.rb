c62f751970b2a4703a53b271236154e20757e719
SupahNickie <nicholascase@live.com>
2013-10-14 21:46:56 -0700

    fixed an accidental Travis error

--- db/migrate/20131015021121_create_projects.rb
+++ db/migrate/20131015021121_create_projects.rb
@@ -1,10 +1,11 @@
 class CreateProjects < ActiveRecord::Migration
   def change
-      create_table :projects do |t|
+    create_table :projects do |t|
       t.string :name
       t.string :technologies_used
 
       t.timestamps
     end
+
   end
 end
